f049e01f3f44e1910235d5514a144f1c659a6c07
appkarsten <app.kar.sten@gmail.com>
2024-12-23 10:19:59 +0100

    solved 4511

--- lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
@@ -4,6 +4,15 @@ class S4511 extends StatelessWidget {
   const S4511({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Helloworld();
+  }
+}
+
+class Helloworld extends StatelessWidget {
+  const Helloworld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello World');
   }
 }
